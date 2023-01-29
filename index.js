8875fd1354e5aa6df8cc7de5dba1a3679352910e
mospit <marcusclarkofers@gmail.com>
2023-01-28 16:14:15 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express  = require  ('express');
+const { createClient } = require  ('@supabase/supabase-js')
+const dotenv = require  ('dotenv');
+const serverless =  require('serverless-http');
+
+dotenv.config();
+const app = express();
+const router = express.Router();
+const PORT = 8080;
+
+app.use(express.json());
+const supabase = createClient(process.env.URL, process.env.API_KEY);
+app.get('/',async (req, res) => {
+    const { data, error } = await supabase
+    .from('data')
+    .select();
+    
+    res.send(data)
+  })
+  
+  app.get('/:id', async (req, res)=> {
+    const { data, error } = await supabase
+.from('data')
+.select();
+
+    const id = req.params.id - 1;
+    res.send(data[id]);
+  });
+
+  app.post('/', async (req, res) => {
+    const sequence = {id: "676"}
+    const { data, error } = await supabase
+    .from('data')
+    .insert({sequence: req.body})
+    
+    res.status(200).send(req.body);
+    res.end();
+  })
+
+app.listen(PORT, () => {
+  console.log(`Listenig on http://localhost:${PORT}`)
+})
