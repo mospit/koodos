9c42f3ffb3737140084afe04e792c64afe572259
Marcus Clark <marcusclarkoffers@gmail.com>
2023-01-28 18:31:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express  = require  ('express');
 const { createClient } = require  ('@supabase/supabase-js')
-const dotenv = require  ('dotenv');
 const serverless =  require('serverless-http');
 
-dotenv.config();
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-const supabase = createClient(process.env.URL, process.env.API_KEY);
+const supabase = createClient(URL,API_KEY);
 app.get('/',async (req, res) => {
     const { data, error } = await supabase
     .from('data')
